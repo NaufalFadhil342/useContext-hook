4940f1185be056a4233b9725c37ac3f7a89cb744
Muhammad Naufal Fadhil Rahmat <naufalfadhil342@gmail.com>
2022-11-03 11:22:43 +0700

    Updates

---
+++ static/js/568.e8c8f056.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkusecontext_hook=self.webpackChunkusecontext_hook||[]).push([[568],{568:function(e,s,n){n.r(s);var t=n(791),u=n(648),h=n(184);s.default=function(){var e=(0,t.useContext)(u._).username;return(0,h.jsxs)("h1",{children:["This is The Home Page and The User is: ",e]})}}}]);
+//# sourceMappingURL=568.e8c8f056.chunk.js.map
