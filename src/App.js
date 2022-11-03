878a7c3484ee9ddf5a9b7d56e6c22ae2450ac61e
Muhammad Naufal Fadhil Rahmat <naufalfadhil342@gmail.com>
2022-11-03 11:21:50 +0700

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 
 import { Context } from './helper/context';
-import Home from './pages/home';
-import Login from './pages/login';
-import Profile from './pages/profile';
 import { Navbar } from './components/Navbar/navbar';
+
+const Home = React.lazy(() => import('./pages/home'));
+const Login = React.lazy(() => import('./pages/login'));
+const Profile = React.lazy(() => import('./pages/profile'));
 
 function App() {
   const [username, setUsername] = useState('FadhilTech');
@@ -15,11 +16,13 @@ function App() {
     <Context.Provider value={{ username, loggedIn, setUsername, setLoggedIn }}>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Suspense fallback={<h1 style={{ marginTop: '4rem' }}>Loading...</h1>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Suspense>
       </Router>
     </Context.Provider>
   );
